d7c5a0415b5d212dac4d4a0823c577832a67c35d
SvsGit <svasya@ro.ru>
2014-01-20 18:41:05 +0400

    LightWeave 1.0.6
    Final Lifeblood Fix thanks to Mavmins and Backburn
    Engineering Gloves, Lifeblood and Xuen moved to Cooldowns

--- lightweave.lua
+++ lightweave.lua
@@ -1,8 +1,8 @@
 -- ProbablyEngine Rotation Packager
 -- Custom Mistweaver Monk Rotation
 -- Created on Dec 31st 2013 9:16 am
--- Modified on Jan 20th 2014 11:00 am
--- Version 1.0.5
+-- Modified on Jan 20th 2014 6:36 pm
+-- Version 1.0.6
 
 ProbablyEngine.library.register('coreHealing', {
   needsHealing = function(percent, count)
@@ -30,24 +30,24 @@ ProbablyEngine.rotation.register_custom(270, '|cFF32ff84Svs LightWeave', {
         '!player.buff(20217).any' -- Blessing of Kings
       }
   },
-  { '115313', 'modifier.lshift', 'ground' }, -- Summon Jade Serpent Statue
   
+  -- Hotkeys
+  { '115313', 'modifier.lshift', 'ground' }, -- Summon Jade Serpent Statue
   { '!115450', 'modifier.lalt', 'mouseover' }, -- Detox Mousever
   
   { '!/targetenemy [noharm]', { '!target.alive', '!target.enemy', '!target.exists' }}, -- Autotarget
   
-  { '115203', 'player.health < 30' }, -- Fortifying Brew
-  
   -- Interrupt
-  { '116705', 'target.interruptAt(50)' }, -- Spear Hand Strike
+  { '116705', 'target.interruptAt(80)' }, -- Spear Hand Strike
   
+  -- Defensives
+  { '115203', 'player.health < 30' }, -- Fortifying Brew
   { '!/run UseItemByName(5512)', 'player.health < 30' }, -- Healthstone
-  
   { '137562', 'player.state.disorient' }, -- Nimble Brew
   { '137562', 'player.state.stun' },
   { '137562', 'player.state.root' },
   { '137562', 'player.state.snare' },
-  { '137562', 'player.state.fear  ' },
+  { '137562', 'player.state.fear' },
 
   -- Legendary Meta Support
   { '!108557', { 'player.buff(137331)', 'target.spell(108557).range', '!modifier.last' }}, -- Jab
@@ -57,9 +57,6 @@ ProbablyEngine.rotation.register_custom(270, '|cFF32ff84Svs LightWeave', {
   { '!100787', { 'player.buff(139597)', 'target.spell(100787).range' }}, -- Tiger Palm/Muscle Memory
   { '123761', { 'player.buff(115867).count >= 2', 'player.mana <= 80' }}, -- Mana Tea
   { '108557', { 'target.spell(108557).range', 'player.mana <= 25', '!modifier.last' }}, -- Jab
-
-  { '121279', { 'player.spell(121279).exists', 'player.spell(121279).cooldown = 0' }, 'player' }, -- Lifeblood, thanks Mavmins and Backburn
-  { '#gloves' },
   { '#trinket1', 'player.mana < 80' },
   { '#trinket2', 'player.mana < 80' },
     
@@ -92,8 +89,13 @@ ProbablyEngine.rotation.register_custom(270, '|cFF32ff84Svs LightWeave', {
   { '101546' },  -- Spinning Crane Kick
   }, 'modifier.multitarget' },
   
-  { '123904', '@coreHealing.needsHealing(60, 4)' }, -- Invoke Xuen, the White Tiger
-  
+  -- Cooldowns
+  {{
+  { '#gloves' },
+  { '/run CastSpellByID(121279)', 'player.spell(121279).cooldown < 1', 'player' }, -- Lifeblood, thanks to Mavmins and Backburn
+  { '123904', '@coreHealing.needsHealing(70, 4)' }, -- Invoke Xuen, the White Tiger
+  }, 'modifier.cooldowns' },
+
   { '115151', { 'tank.health < 99', 'tank.spell(115151).range' }, 'tank' }, -- Renewing Mist
   { '115151', { 'lowest.health < 99', '!lowest.buff(119611)', 'lowest.spell(115151).range' }, 'lowest' }, -- Renewing Mist
   { '115151', 'player.chi < 5' }, -- Renewing Mist
